499f1ee1e22c678438093e5cf3342df836d8188d
Youngjin Mo <creator.youngjin@gmail.com>
2019-08-28 19:53:15 +0900

    PM/AM 시간대 분리

--- app/index.js
+++ app/index.js
@@ -6,16 +6,17 @@ let myClock = document.getElementById("myClock");
 clock.granularity = "seconds";
 
 clock.ontick = function(evt) {
-    myClock.text = ("0" + evt.date.getHours()).slice(-2) + ":" +
+    let hours = evt.date.getHours();
+
+    // AM/PM 12시간 나누기
+    if(hours>12){
+        hours = hours - 12;
+    }
+
+    myClock.text = ("0" + hours).slice(-2) + ":" +
     ("0" + evt.date.getMinutes()).slice(-2) + ":" +
     ("0" + evt.date.getSeconds()).slice(-2);
 
     console.log(evt.date.toString());
-    console.log(evt.getHours());
+    console.log(hours);
 }
-
-clock.ontick = function(evt) {
-    myClock.text = ("0" + evt.date.getHours()).slice(-2) + ":" +
-                        ("0" + evt.date.getMinutes()).slice(-2) + ":" +
-                        ("0" + evt.date.getSeconds()).slice(-2);
-  };
