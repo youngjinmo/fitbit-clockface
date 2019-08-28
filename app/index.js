ee3f32d94b288edce0ddb40d7f35736fdfacb005
Youngjin Mo <creator.youngjin@gmail.com>
2019-08-28 19:27:24 +0900

    기초적인 clock 구현

--- app/index.js
+++ app/index.js
@@ -1,1 +1,21 @@
-console.log('Hello world!');
+import document from "document";
+import clock from "clock";
+
+let myClock = document.getElementById("myClock");
+
+clock.granularity = "seconds";
+
+clock.ontick = function(evt) {
+    myClock.text = ("0" + evt.date.getHours()).slice(-2) + ":" +
+    ("0" + evt.date.getMinutes()).slice(-2) + ":" +
+    ("0" + evt.date.getSeconds()).slice(-2);
+
+    console.log(evt.date.toString());
+    console.log(evt.getHours());
+}
+
+clock.ontick = function(evt) {
+    myClock.text = ("0" + evt.date.getHours()).slice(-2) + ":" +
+                        ("0" + evt.date.getMinutes()).slice(-2) + ":" +
+                        ("0" + evt.date.getSeconds()).slice(-2);
+  };
