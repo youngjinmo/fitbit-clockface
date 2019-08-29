91ceacffae9b5a3374a16aa8977d272a7ab00b87
Youngjin Mo <creator.youngjin@gmail.com>
2019-08-29 12:31:23 +0900

    steps 띄우고, circle opacity적용

--- app/index.js
+++ app/index.js
@@ -4,29 +4,30 @@ import { preferences } from "user-settings";
 import { today, goals } from "user-activity";
 
 
-// 시간 보여주는 기능
 let myClock = document.getElementById("myClock");
+let steps = document.getElementById("steps");
+
+
+steps.text = today.local.steps || 0;
+
 
 clock.granularity = "seconds";
-
-clock.ontick = function(evt) {
+clock.ontick = (evt) => {
+    
     let hours = evt.date.getHours();
     let minutes = evt.date.getMinutes();
     let seconds = evt.date.getSeconds();
+
 
     // AM/PM 12시간 나누기
     if(hours>12){
         hours = hours - 12;
     }
 
-    myClock.text = ("0" + hours).slice(-2) + ":" +
-    ("0" + evt.date.getMinutes()).slice(-2) + ":" +
-    ("0" + evt.date.getSeconds()).slice(-2);
-
     // 시간 카운트가 한 자리수일 때 앞에 0 추가
-    if(hours<10){
-        hours = "0"+hours;
-    }
+    // if(hours<10){
+    //     hours = "0"+hours;
+    // }
     if(minutes<10) {
         minutes = "0"+minutes;
     }
